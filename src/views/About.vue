0ad9b93eb8eb46506850494094c4da7454a9cd78
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-06-07 13:11:55 +0200

    Added Typescript and @fullcalendar plus plugins

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<FullCalendar :options="calendarOptions" />
 </template>
+<script>
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+export default {
+	components: {
+		FullCalendar // make the <FullCalendar> tag available
+	},
+	data() {
+		return {
+				calendarOptions: {
+				plugins: [ dayGridPlugin, interactionPlugin ],
+				initialView: 'dayGridMonth'
+			}
+		}
+	}
+}
+</script>
