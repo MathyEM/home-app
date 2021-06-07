f85fee003a4004ac23760a37ca1fac26f1bf288f
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-06-07 15:41:45 +0200

    Set up vuex for fullcalendar and added locale options

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    events: [
+      {
+        title: "Event 1",
+        start: new Date('2021-06-07T14:00:00').toISOString().replace(/\.[0-9]{3}/, ''),
+        end: new Date('2021-06-09T14:30:00').toISOString().replace(/\.[0-9]{3}/, '')
+      },
+      {
+        title: "Event 2",
+        start: '2021-06-08'
+      }
+    ],
+  },
+  getters: {
+    events: state => state.events,
   },
   mutations: {
   },
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,20 +2,43 @@
 	<FullCalendar :options="calendarOptions" />
 </template>
 <script>
+import { mapGetters } from 'vuex'
+
 import FullCalendar from '@fullcalendar/vue'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import daLocale from '@fullcalendar/core/locales/da';
 
 export default {
 	components: {
 		FullCalendar // make the <FullCalendar> tag available
 	},
+	computed: {
+		...mapGetters(['events']),
+
+		calendarOptions() {
+			return {
+				plugins: [ dayGridPlugin, interactionPlugin ],
+				initialView: 'dayGridMonth',
+				eventClick: this.handleDateClick,
+				events: this.events,
+				locale: daLocale,
+				eventTimeFormat: {
+					hour: '2-digit',
+					minute: '2-digit',
+					meridiem: false
+				}
+			}
+		}
+	},
 	data() {
 		return {
-				calendarOptions: {
-				plugins: [ dayGridPlugin, interactionPlugin ],
-				initialView: 'dayGridMonth'
-			}
+			
+		}
+	},
+	methods: {
+		handleDateClick: function(arg) {
+			console.log(arg.event.start.toISOString())
 		}
 	}
 }
