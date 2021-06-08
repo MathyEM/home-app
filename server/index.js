c367529e7d5bdf9c4460715f370f9c420aae1272
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-06-08 15:03:29 +0200

    Removed old unused code

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-// const mongoose = require('mongoose')
-// const bodyParser = require('body-parser')
 const caldav = require('./caldav')
 const routes = require('./routes')
 
@@ -14,21 +12,6 @@ app.use(express.json());
 app.use('/', routes)
 
 const port = 3000
-
-// const uri = process.env.DB_SERVER;
-// const db_user = process.env.DB_USER;
-// const db_pass = process.env.DB_PASS;
-// mongoose.connect(uri, {
-// 	user: db_user,
-// 	pass: db_pass,
-// 	useNewUrlParser: true,
-// 	useUnifiedTopology: true
-// })
-// .then(() => {
-// 	console.log('MongoDB Connected')
-// })
-// .catch(err => console.log(err))
-
 
 app.listen(port, () => {
 	console.log(`Server started on http://localhost:${port}`)
