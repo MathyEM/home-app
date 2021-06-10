b7d4d4f26365c25d4db8d93692b005bdf413a137
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-06-10 12:04:46 +0200

    Added TodoList components

---
+++ src/components/todoList/AddTodo.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="new-todo">
+        <input 
+            type="text"
+            name="new-todo"
+            id="new-todo"
+            placeholder="Tilføj en ting"
+			v-model="newTodo"
+        >
+    </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex';
+
+export default {
+	name: "AddTodo",
+	components: {
+
+	},
+	data() {
+		return {
+			newTodoInput: "",
+		}
+	},
+	computed: {
+		...mapMutations(['addTodo']),
+		newTodo: {
+			get () {
+				return this.newTodoInput
+			},
+			set () {
+				this.addTodo(newTodoInput)
+				return this.newTodoInput = ""
+			}
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
