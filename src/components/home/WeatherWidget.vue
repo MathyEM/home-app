1277ba9312232adab4943d3a9892308d2385acdf
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-07-02 17:00:25 +0200

    Initial display for weather data

--- src/components/home/WeatherWidget.vue
+++ src/components/home/WeatherWidget.vue
@@ -1,22 +1,33 @@
 <template>
 	<div class="weather-widget-container">
-		
+		<div v-if="currentWeather" class="current-weather">
+            <p>{{ convertUnixDate(currentWeather.dt) }}</p>
+            <p>temp: {{ currentWeather.temp }} C</p>
+        </div>
 	</div>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from 'vue'
 import { mapActions, mapGetters } from 'vuex';
 
 export default Vue.extend({
 	name: 'WeatherWidget',
 	components: {
+
 	},
 	computed: {
-		...mapGetters(['weatherCache'])
+		...mapGetters(['weatherCache', 'currentWeather', 'hourlyWeather', 'dailyWeather']),
+        
 	},
 	methods: {
-		...mapActions(['searchLocation'])
+		...mapActions(['searchLocation']),
+        convertUnixDate(unixDate) {
+            const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' }
+            let date = new Date(unixDate*1000).toLocaleString('da-DK', options)
+            return date
+
+        },
 
 	},
 	async beforeCreate() {
--- src/services/weatherapi.js
+++ src/services/weatherapi.js
@@ -7,7 +7,7 @@ async function getWeather(payload) {
     payload
     const lat = "55.490398" //payload.lat
     const lon = "9.47216" //payload.lon
-    return await axios.get(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`)
+    return await axios.get(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&lang=da&units=metric`)
 }
 
 export {
