5c699ca7bf51811b3d482cbf58579c11bf12b807
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-07-07 15:40:37 +0200

    Set up layout for front page widgets

--- src/components/home/WeatherWidget.vue
+++ src/components/home/WeatherWidget.vue
@@ -1,8 +1,11 @@
 <template>
-	<div class="weather-widget-container">
-		<div v-if="currentWeather" class="current-weather">
-            <p>{{ convertUnixDate(currentWeather.dt) }}</p>
-            <p>temp: {{ currentWeather.temp }} C</p>
+	<div class="weather-widget widget">
+        <h3>Vejr</h3>
+		<div v-if="currentWeather && dailyWeather" class="current-weather">
+            <h5 class="location">Kolding</h5>
+            <h4 class="temperature">{{ currentWeather.temp.toFixed(0) }}°</h4>
+            <p>{{ dailyWeather[0].temp.max.toFixed(0) + "°" + "/" + dailyWeather[0].temp.min.toFixed(0) + "°" /*convertUnixDate(currentWeather.dt)*/ }}</p>
+            <img class="weather-icon" :src="`http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`" alt="">
         </div>
 	</div>
 </template>
@@ -35,3 +38,14 @@ export default Vue.extend({
 	},
 });
 </script>
+
+<style lang="scss" scoped>
+    .weather-widget {
+        h3 {
+            margin-bottom: 0.5rem;
+        }
+        .weather-icon {
+            filter: drop-shadow(0px 0px 1px rgba(0,0,0,0.5));
+        }
+    }
+</style>
