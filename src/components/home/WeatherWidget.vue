a2e7cace9e911841ab55f0bcee1eb038d3505840
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-07-08 19:03:40 +0200

    Added daily weather

--- src/components/home/WeatherWidget.vue
+++ src/components/home/WeatherWidget.vue
@@ -1,18 +1,33 @@
 <template>
 	<div class="weather-widget widget">
         <h3>Vejr</h3>
-		<CurrentWeather />
+		<template v-if="currentWeather && dailyWeather">
+            <CurrentWeather
+                    
+                        :current="currentWeather"
+                        :daily="dailyWeather"
+                    />
+        </template>
+        <template v-if="dailyWeather">
+            <DailyWeather
+                v-for="(daily, index) in dailyWeather"
+                :daily="daily"
+                :key="index"
+            />
+        </template>
 	</div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex';
 import CurrentWeather from '@/components/weather/CurrentWeather.vue'
+import DailyWeather from '@/components/weather/DailyWeather.vue'
 
 export default {
 	name: 'WeatherWidget',
 	components: {
-        CurrentWeather
+        CurrentWeather,
+        DailyWeather,
 	},
 	computed: {
 		...mapGetters(['weatherCache', 'currentWeather', 'hourlyWeather', 'dailyWeather']),
