a8ff951f0a7b0072a0af9d8c4e00806507353c5d
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-07-08 12:27:58 +0200

    Added hourly weather

---
+++ src/components/weather/HourWeather.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="weather-hour">
+        <div class="date-time">
+            <h5 class="date">{{ date }}</h5>
+            <h5 class="time">{{ time }}</h5>
+        </div>
+        <img
+            :src="`http://openweathermap.org/img/wn/${hour.weather[0].icon}@2x.png`"
+            :alt="'vejrikon med ' + hour.weather[0].description"
+            class="icon"
+        >
+        <div class="temp-rain">
+            <p class="temp">{{ hour.temp.toFixed(0) + "°" }}</p>
+            <p v-if="hour.rain" class="rain">{{ hour.rain['1h'].toString().replace('.', ',') + "mm" }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HourWeather',
+    props: {
+        hour: {
+            type: Object,
+            required: true,
+        }
+    },
+    computed: {
+        date() {
+            const options = { month: '2-digit', day: '2-digit' }
+            let date = new Date(1000*this.hour.dt).toLocaleString('da-DK', options)
+            date = date.replace('.','/')
+
+            return date
+        },
+        time() {
+            const options = { hour: '2-digit', minute: '2-digit' }
+            let time = new Date(1000*this.hour.dt).toLocaleTimeString('da-DK', options)
+            time = time.replace('.', ':').replace(' ', '\n- ')
+
+            return time
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.weather-hour {
+    margin-bottom: 5px;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    align-items: center;
+    border-bottom: 1px solid darkgray;
+}
+.date-time {
+    justify-self: flex-start;
+}
+.icon {
+    height: 50px;
+    justify-self: flex-start;
+}
+.temp-rain {
+    justify-self: center;
+    font-size: 0.9rem;
+}
+</style>
