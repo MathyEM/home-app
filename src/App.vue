5d30770c63f1f7b2d5a09295fb91f3390df623c2
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-07-13 11:17:46 +0200

    Added small weather widget to header

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,75 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/calendar">Kalender</router-link> |
-      <router-link to="/todolist">Huskeseddel</router-link> |
-      <router-link to="/hueapp">Hue App</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+		<div class="header">
+			<div class="header-weather">
+				<WeatherWidgetSmall />
+			</div>
+			<div id="nav">
+				<router-link to="/">Home</router-link> |
+				<router-link to="/calendar">Kalender</router-link> |
+				<router-link to="/todolist">Huskeseddel</router-link> |
+				<router-link to="/hueapp">Hue App</router-link>
+			</div>
+		</div>
+		<router-view/>
+	</div>
 </template>
+
+<script>
+import WeatherWidgetSmall from '@/components/weather/WeatherWidgetSmall.vue'
+export default {
+	components: {
+		WeatherWidgetSmall,
+	}
+}
+</script>
 
 <style lang="scss">
 * {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
 }
 
 :root {
-  height: 100vh;
+	height: 100vh;
 }
 
 body {
-  height: 100%;
+	height: 100%;
 }
 
 #app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  padding: 0.2rem 0.5rem;
-  color: #2c3e50;
-  display: grid;
-  grid-template-rows: auto 1fr;
-  height: 100%;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	padding: 0.2rem 0.5rem;
+	color: #2c3e50;
+	display: grid;
+	grid-template-rows: auto 1fr;
+	height: 100%;
 }
 
 #app, input#updatedTodo {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+.header-weather {
+	display: inline-block;
+	vertical-align: middle;
 }
 
 #nav {
-  padding: 30px;
+	display: inline-block;
+	padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+	a {
+		font-weight: bold;
+		color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+		&.router-link-exact-active {
+			color: #42b983;
+		}
+	}
 }
 </style>
---
+++ src/components/weather/WeatherWidgetSmall.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="current-weather-small-container">
+            <div class="current-weather-small-wrapper">
+                <h5 class="location">Kolding</h5>
+                <div class="icon-temp-row">
+                    <img class="weather-icon icon-shadow" :src="`http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`" alt="">
+                    <div class="temperature-container">
+                        <h4 class="temperature">{{ currentWeather.temp.toFixed(0) }}°</h4>
+                        <p class="min-max">{{ dailyWeather[0].temp.max.toFixed(0) + "°" + "/" + dailyWeather[0].temp.min.toFixed(0) + "°" }}</p>
+                    </div>
+                </div>
+                <p class="weather-description">{{ currentWeather.weather[0].description }}</p>
+            </div>
+        </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+    name: 'WeatherWidgetSmall',
+    props: {
+        
+    },
+    components: {
+
+    },
+    computed: {
+        ...mapGetters(['currentWeather', 'dailyWeather'])
+    }
+}
+</script>
+
+<style>
+
+</style>
