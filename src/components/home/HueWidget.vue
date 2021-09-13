4a40c9f8522dba0b32ffa4e389ccbacedf6ed20e
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-09-13 17:49:20 +0200

    Updated HueWidget to use updated HueEntitySmall

--- src/components/home/HueWidget.vue
+++ src/components/home/HueWidget.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="calendar-widget-container widget">
+    <div class="hue-widget-container widget">
         <h3>Lys</h3>
         <template v-for="group, g_index in groups">
-            <HueEntitySmall :key="g_index" :isGroup="true" :id="g_index" />
+            <HueEntitySmall :colorTemperatureStep="34.7" :brightnessStep="15.8125" :key="g_index" :isGroup="true" :id="g_index" />
         </template>
     </div>
 </template>
@@ -49,24 +49,85 @@ export default {
 </script>
 
 <style lang="scss">
-    .calendar-widget-container {
+    .hue-widget-container {
         .small-entity-wrapper {
-            padding: 0.5rem 1rem;
-            border-radius: 0;
             box-shadow: none;
             background: initial;
             width: 100%;
-            border-bottom: 1px solid $dark-gray;
-            margin: 0;
 
             h3 {
                 margin: 0;
             }
 
-            .control-wrapper .on-off-switch {
-                margin: 30px 0;
-            }
+            .control-wrapper {
+				grid-template-columns: 0.7fr 2fr;
+
+				.switch-container.on-off-switch {
+					height: auto;
+
+					button {
+						font-size: 1.5rem;
+
+						&.on, &:active {
+							background-color: #c1c1c1;
+							border-color: #b2b2b2;
+							border-width: 2px;
+						}
+					}
+				}
+
+				.color-title-wrapper {
+					.single-control-wrapper {
+						padding-top: 0.5rem;
+						
+						input {
+							padding: 0.5rem;
+							width: 100%;
+							border: none;
+						}
+					}
+				}
+			}
+			
         }
+
+		$thumb-width: 24px;
+		$thumb-height: 28px;
+		$track-height: 18px;
+		$thumb-margin-top: ( $thumb-height * -0.52 ) + ( $track-height * 0.5 );
+
+		* {
+			--thumb-color: whitesmoke;
+		}
+
+		.color-temperature, .brightness {
+			position: relative; // This makes the slider clickable even when the color wheel is overlapping
+			padding-top: 0.5rem;
+			padding-bottom: 0.5rem;
+			-webkit-appearance: none;
+			background: transparent;
+
+			&:focus {
+				outline: none;
+			}
+
+			&::-webkit-slider-thumb {
+				-webkit-appearance: none;
+				border: 1px solid #e2e2e2;
+				width: $thumb-width;
+				height: $thumb-height;
+				border-radius: 0px;
+				background: var(--thumb-color);
+				cursor: pointer;
+				justify-self: center;
+				margin-top: $thumb-margin-top; /* You need to specify a margin in Chrome, but in Firefox and IE it is automatic */
+			}
+			&::-webkit-slider-runnable-track {
+				cursor: pointer;
+				height: $track-height;
+				border: none;
+				border-radius: 0px;
+			}
+		}
     }
-    
 </style>
